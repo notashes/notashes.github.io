78aa516a06ff08f23f7e74c7836a72420c394e3d
notashes <edgerunnergit@riseup.net>
2025-08-03 18:52:27 +0530

    small update

--- src/site.config.ts
+++ src/site.config.ts
@@ -75,7 +75,7 @@ export const theme: ThemeUserConfig = {
       // }
     ],
     /** Enable displaying a “Astro & Pure theme powered” link in your site’s footer. */
-    credits: true,
+    credits: false,
   },
 
   content: {
